e92696d8d8ae3a6bd2c066b78f9f9e43c9cbef10
onmedicines <semwalanurag26@gmail.com>
2024-12-30 11:13:23 +0530

    Setup mongodb

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`server started on port ${port}.`);
 });
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose
+  .connect(process.env.ATLAS_URI)
+  .then(() => {
+    console.log("Connected to MongoDB Atlas.");
+  })
+  .catch(() => {
+    console.log("Could not connect to MongoDB Atlas!");
+  });
+
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to Book records</br>");
+});
