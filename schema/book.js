6a6ec8e305281f49819db33e2cfce22cea3b6230
onmedicines <semwalanurag26@gmail.com>
2024-12-31 16:44:48 +0530

    feat - Add essential fields in book schema

--- schema/book.js
+++ schema/book.js
@@ -3,8 +3,11 @@ import mongoose from "mongoose";
 const bookSchema = mongoose.Schema({
   name: { type: String, required: true },
   pages: { type: Number, required: true },
-  genre: { type: String, required: true },
-  userId: { type: String, required: true },
+  author: { type: String, required: true },
+  genre: [{ type: String, required: true }],
+  username: { type: String, required: true },
+  rating: { type: Number, required: true },
+  review: { type: String, required: false },
 });
 
 export const Book = mongoose.model("Book", bookSchema);
